6769343bdb3ea519213092c0f752d9e47e8d3834
Ntwali B <ntwali.bashige@gmail.com>
2024-07-19 08:02:24 +0100

    Forgot to push untracked files.

---
+++ ckanext/googleanalytics/helpers.py
@@ -0,0 +1,12 @@
+from .utils.numerize import numerize
+from ckan.plugins import toolkit
+
+
+def get_package_stats(package_id):
+    package_stat = toolkit.get_action('package_stats')({}, {'package_id': package_id})
+    return numerize(int(package_stat))
+
+
+def get_resource_stats(resource_id):
+    resource_stat = toolkit.get_action('resource_stats')({}, {'resource_id': resource_id})
+    return numerize(int(resource_stat))
